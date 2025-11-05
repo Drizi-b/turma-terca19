b53fd56f709f2ea6d7abffcf1419b969f4340134
Adriana Balon <drizi.b.oliveira@gmail.com>
2025-11-04 22:36:38 -0300

    Regularizacao DOM

--- frontEnd/DOM/aula-dom02/verificadorDeIdade/src/js/script.js
+++ frontEnd/DOM/aula-dom02/verificadorDeIdade/src/js/script.js
@@ -1,11 +1,11 @@
 const botao = document.querySelector(".btn");
 
-function VerificaIdade() ;{
-    let anoAtual = nem Date().getFullYear();
+function VerificarIdade() {
+    let anoAtual = new Date().getFullYear();
     let anoNascimentoForm = document.querySelector("#txtano");
     let resultado = document.querySelector("#resultado");
 
-    if (anoNascimentoForm.ariaValueMax.length === 0 || anoNascimentoForm.value > anoAtual) {
+    if (anoNascimentoForm.value.length === 0 || anoNascimentoForm.value > anoAtual) {
         alert("[ERRO] Verifique os dados e tente novamente!");
     } else {
         let formSexo = document.getElementsByName("radsex");
@@ -29,30 +29,28 @@ function VerificaIdade() ;{
                 img.setAttribute("src", "./assets/adulto-f.jpeg");                
             } else {
                 faixaEtaria = "uma idosa";
-                img.setAttribute("src", "./assets/idoso-f.jpeg");                
-
+                img.setAttribute("src", "./assets/idoso-f.jpeg");
             }
-        }
+        } else {
             if (idade >= 0 && idade < 4) {
-                faixaEtaria = "um bebê";
-                img.setAttribute("src", "./assets/bebe-m.jpeg");
+                faixaEtaria = "um bebê"
+                img.setAttribute("src", "./assets/bebe-m.jpeg")
             } else if (idade >= 4 && idade < 13) {
-                faixaEtaria = "um menino";
-                img.setAttribute("src", "./assets/crianca-m.jpeg");                
+                faixaEtaria = "um menino"
+                img.setAttribute("src", "./assets/crianca-m.jpeg")               
             } else if (idade >= 13 && idade < 21) {
-                faixaEtaria = "um adolescente";
-                img.setAttribute("src", "./assets/adolescente-m.jpeg");                
+                faixaEtaria = "um adolescente"
+                img.setAttribute("src", "./assets/adolescente-m.jpeg")              
             } else if (idade <= 50) {
-                faixaEtaria = "um adulto";
-                img.setAttribute("src", "./assets/adulto-m.jpeg");                
+                faixaEtaria = "um adulto"
+                img.setAttribute("src", "./assets/adulto-m.jpeg")             
             } else {
-                faixaEtaria = "um idoso";
-                img.setAttribute("src", "./assets/idoso-m.jpeg");                
-
+                faixaEtaria = "um idoso"
+                img.setAttribute("src", "./assets/idoso-m.jpeg")            
             }
         }
-    resultado.innerHTML = "Detectamos ${faixaEtaria} com ${idade} anos.";
-    resultado.appendChild(img);
+    resultado.innerHTML = `Detectamos ${faixaEtaria} com ${idade} anos.`;
+    resultado.appendChild(img)
     }
 }
-botao.addEventListener("click", VerificaIdade);
+botao.addEventListener("click", VerificarIdade);
